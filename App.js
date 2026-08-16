f8ba306b4491fef8287f2e840d3892744da93d69
Jahanzaib Channar <jahanzaibchannar954@gmail.com>
2026-08-16 15:24:19 +0500

    make small changes

--- App.js
+++ App.js
@@ -1,39 +1,39 @@
 
 
 
-const city = "Karachi"
+// const city = "Karachi"
 
 
-const cityurl = `https://geocoding-api.open-meteo.com/v1/search?name=${city}`;
+// const cityurl = `https://geocoding-api.open-meteo.com/v1/search?name=${city}`;
 
-async function Weather() {
+// async function Weather() {
 
-    try {
+//     try {
 
-        let load = await fetch(cityurl);
+//         let load = await fetch(cityurl);
 
-        let geodata = await load.json();
+//         let geodata = await load.json();
 
-        let latitude = geodata.results[0].latitude
-        let longitude = geodata.results[0].longitude
+//         let latitude = geodata.results[0].latitude
+//         let longitude = geodata.results[0].longitude
 
-        // const url =
-        //     `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m`;
+//         // const url =
+//         //     `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m`;
 
 
-        const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code&timezone=auto`;
+//         const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m,weather_code&timezone=auto`;
 
-        let weatherData = await fetch(url);
+//         let weatherData = await fetch(url);
 
-        let data = await weatherData.json();
+//         let data = await weatherData.json();
 
-        console.log(data.current.temperature_2m)
+//         console.log(data.current.temperature_2m)
 
-    } catch (error) {
+//     } catch (error) {
 
-        console.log("error", error)
-    }
+//         console.log("error", error)
+//     }
 
-}
+// }
 
-Weather();
+// Weather();
